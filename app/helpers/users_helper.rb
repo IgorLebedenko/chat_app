85942e24ea62f4b820138d3666885cec8dbf61ab
IgorLebedenko <igor777lebedenko@yandex.ru>
2016-03-17 15:19:54 +0300

    finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def avatar_for(user, options = { size: 70 })
+    first_letter = user.name.lstrip[0].upcase
+    image_path = "letters/#{first_letter}.png"
+    image_tag(image_path, class: "avatar", size: "#{options[:size]}")
+  end
 end
